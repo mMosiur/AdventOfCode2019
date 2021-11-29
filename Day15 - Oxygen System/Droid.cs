44742be3f99959b70670eb48976c48b1215a4598
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-11-29 19:37:10 +0100

    Upgrade Day15 puzzle to dotnet6.0

--- Day15 - Oxygen System/Droid.cs
+++ Day15 - Oxygen System/Droid.cs
@@ -1,82 +1,70 @@
 using System;
 using System.Linq;
 
-namespace Day15
+namespace Day15;
+
+public class Droid : ICloneable
 {
-	public struct Point
+	private readonly IntcodeMachine _machine;
+	public Point Position { get; private set; }
+	public int StepsTaken { get; private set; }
+
+	private Droid(IntcodeMachine machine, Point position, int stepsTaken)
 	{
-		public int X { get; set; }
-		public int Y { get; set; }
-		public Point(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
+		_machine = machine;
+		Position = position;
+		StepsTaken = stepsTaken;
 	}
 
-	public class Droid : ICloneable
+	public Droid(long[] program)
 	{
-		private readonly IntcodeMachine _machine;
-		public Point Position { get; private set; }
-		public int StepsTaken { get; private set; }
-
-		private Droid(IntcodeMachine machine, Point position, int stepsTaken)
-		{
-			_machine = machine;
-			Position = position;
-			StepsTaken = stepsTaken;
-		}
-
-		public Droid(long[] program)
-		{
-			_machine = new IntcodeMachine(
-				program,
-				Enumerable.Empty<long>(),
-				null
-			);
-			Position = new Point(0, 0);
-			StepsTaken = 0;
-		}
-
-		public long Move(int dir)
-		{
-			_machine.Input?.Enqueue(dir);
-			Point target = Position;
-			switch (dir)
-			{
-				case 1:
-					target.Y--;
-					break;
-				case 2:
-					target.Y++;
-					break;
-				case 3:
-					target.X--;
-					break;
-				case 4:
-					target.X++;
-					break;
-				default: throw new Exception("?");
-			}
-			long output = _machine.RunToFirstOutput();
-			switch (output)
-			{
-				case 0:
-					break;
-				case 1:
-					Position = target;
-					break;
-				case 2:
-					Position = target;
-					break;
-				default: throw new Exception("?");
-			}
-			StepsTaken++;
-			return output;
-		}
-
-		object ICloneable.Clone() => Clone();
-
-		public Droid Clone() => new(_machine.Clone(), Position, StepsTaken);
+		_machine = new IntcodeMachine(
+			program,
+			Enumerable.Empty<long>(),
+			null
+		);
+		Position = new Point(0, 0);
+		StepsTaken = 0;
 	}
+
+	public long Move(int dir)
+	{
+		_machine.Input?.Enqueue(dir);
+		Point target = Position;
+		switch (dir)
+		{
+			case 1:
+				target.Y--;
+				break;
+			case 2:
+				target.Y++;
+				break;
+			case 3:
+				target.X--;
+				break;
+			case 4:
+				target.X++;
+				break;
+			default: throw new Exception("?");
+		}
+		long output = _machine.RunToFirstOutput();
+		switch (output)
+		{
+			case 0:
+				break;
+			case 1:
+				Position = target;
+				break;
+			case 2:
+				Position = target;
+				break;
+			default: throw new Exception("?");
+		}
+		StepsTaken++;
+		return output;
+	}
+
+	object ICloneable.Clone() => Clone();
+
+	public Droid Clone() => new(_machine.Clone(), Position, StepsTaken);
 }
