3ea4e00a6f89a1370ce9eef69c16f123988b60c3
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-11-29 18:38:01 +0100

    Upgrade Day08 puzzle to dotnet6.0

--- Day08 - Space Image Format/Program.cs
+++ Day08 - Space Image Format/Program.cs
@@ -2,37 +2,31 @@ using System;
 using System.IO;
 using System.Linq;
 
-namespace Day08
+using Day08;
+
+const string DEFAULT_INPUT_FILEPATH = "input.txt";
+const int IMAGE_WIDTH = 25;
+const int IMAGE_HEIGHT = 6;
+
+string filepath = args.Length > 0 ? args[0] : DEFAULT_INPUT_FILEPATH;
+char[] encodedImage = File.ReadAllText(filepath).Trim().ToCharArray();
+SpaceImage image = new(encodedImage, IMAGE_WIDTH, IMAGE_HEIGHT);
+int min = int.MaxValue;
+int minIndex = -1;
+for (int i = 0; i < image.Layers.Count; i++)
 {
-	class Program
+	int count = image.Layers[i].Count(c => c == '0');
+	if (count < min)
 	{
-		const string INPUT_FILEPATH = "input.txt";
-		const int IMAGE_WIDTH = 25;
-		const int IMAGE_HEIGHT = 6;
-
-		static void Main()
-		{
-			char[] encodedImage = File.ReadAllText(INPUT_FILEPATH).Trim().ToCharArray();
-			SpaceImage image = new(encodedImage, IMAGE_WIDTH, IMAGE_HEIGHT);
-			int min = int.MaxValue;
-			int minIndex = -1;
-			for (int i = 0; i < image.Layers.Count; i++)
-			{
-				int count = image.Layers[i].Count(c => c == '0');
-				if (count < min)
-				{
-					min = count;
-					minIndex = i;
-				}
-			}
-			int ones = image.Layers[minIndex].Count(c => c == '1');
-			int twos = image.Layers[minIndex].Count(c => c == '2');
-			int part1 = ones * twos;
-			Console.WriteLine($"Part 1: {part1}");
-
-			Console.WriteLine("Part 2 message:");
-			SpaceImageLayer visibleImage = image.GetVisibleImage();
-			Console.WriteLine(visibleImage.GetImage());
-		}
+		min = count;
+		minIndex = i;
 	}
 }
+int ones = image.Layers[minIndex].Count(c => c == '1');
+int twos = image.Layers[minIndex].Count(c => c == '2');
+int part1 = ones * twos;
+Console.WriteLine($"Part 1: {part1}");
+
+Console.WriteLine("Part 2 message:");
+SpaceImageLayer visibleImage = image.GetVisibleImage();
+Console.WriteLine(visibleImage.GetImage());
